1828ebd8f853dc2f8d282c965342b9cc75483a00
Хлоя <allenzj@cardiff.ac.uk>
2018-11-27 10:45:34 +0000

    Added light detection and feedback loop, requires independent thread while running, needs to become thread safe.
    Consideration of content of Radio messages is required currently, this is not super important as I2C will be implemented before completion.
    
    Also added Radio.config settings

--- waterSense.py
+++ waterSense.py
@@ -2,18 +2,62 @@ from microbit import *
 import radio
 display.scroll('ON')
 
-radio.on()
-ans = radio.receive()
+#Unique addressing for microprocessor radios
+radio.config(channel=32,address=00000001,group=2)
 
-#Use async to make this a seperate thread
+"""Use above settings on other radios:
+    -Channel always 32
+    -Address reflective of role
+        -Last bit addresses indicate slaves
+        -[:-2] bit address indicate feedback
+        -[:-3] bit address indicates master unit
+    -grouping
+        -group 2 for slaves
+        -group 0 for controller
+        -group 1 for actors/feedback controllers
+
+"""
+
+#Turn radio on
+radio.on()
+
+#Var x used for water limit, out of possible 1024
+x = 450
+#Var y used for light levels, out of possible 1024
+y = 100
+yList = []
+
+#Use async to make this a seperate thread in future
 while True:
+    #listen for reset command
+    ans = radio.receive()
+    #Water probe is on pin 1
     water = pin1.read_analog()
-    if water < 500:
-        display.scroll(water)
-    elif water >= 500:
+    light = pin0.read_analog()
+    
+    #If water level below x, do:
+    if water < x:
+        radio.send(water)
+    elif water >= x:
         display.scroll("Water me!")
-        
+        radio.send(water)
+    
+    """
+    #Lines 46 - 56 need to become thread independent and safe
+    if light < y:
+        ylist.append(y)
+        wait(60000)
+        radio.send("y: ",y)
+    elif light == 0:
+        wait(60000)
+        radio.send("ERROR, LIGHT")
+    else:
+        ylist.append(y)
+        wait(60000)
+        radio.send("ERROR, LOW LIGHT")
+        """
+    
     #Needs boolean to detect water existance (if water == True)
     if ans == 'reset':
-        display.scroll("BYE")
+        display.scroll("CYCLING")
         reset()
