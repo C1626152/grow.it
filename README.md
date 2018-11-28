49bbb871500e8c3a51768cad72397e3c21e4c325
Хлоя <allenzj@cardiff.ac.uk>
2018-11-28 13:34:52 +0000

    Updated waterSense.py to be more object oriented

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # grow.it
 
 This is a project to produce an open source growing system that is easy to use, set up and expand without needing expensive modules.
+
+##Current Radio communication plan
+-Channel always 32
+    -Address reflective of role
+        -Last bit addresses indicate slaves
+        -[:-2] bit address indicate feedback
+        -[:-3] bit address indicates master unit
+    -grouping
+        -group 2 for slaves
+        -group 0 for controller
+        -group 1 for actors/feedback controllers
+
+
+##Light detection:
+	This element of the system is still being developed and requires more work before being rolled out. The function detectLight() will be an independently threaded application, which should monitor light levels for one [1] hour and then send a list of light levels measured every 60s to the master controller. The remainder of processing numbers, averaging and acting on the data will be done on the master controller.
+
+This multithreading can be achieved by downloading and installing async module "https://github.com/peterhinch/micropython-async/blob/master/TUTORIAL.md#01-installing-uasyncio-on-bare-metal"
+
--- waterSense.py
+++ waterSense.py
@@ -1,11 +1,18 @@
 from microbit import *
-import radio
+import radio, thread, time
+
+# Var x used for water limit, out of possible 1024
+x = 450
+# Var y used for light levels, out of possible 1024
+y = 100
+yList = []
+
 display.scroll('ON')
 
-#Unique addressing for microprocessor radios
-radio.config(channel=32,address=00000001,group=2)
-
-"""Use above settings on other radios:
+# Unique addressing for microprocessor radios
+radio.config(channel=32, address=0x00000001, group=2)
+"""
+Use above settings on other radios:
     -Channel always 32
     -Address reflective of role
         -Last bit addresses indicate slaves
@@ -15,49 +22,56 @@ radio.config(channel=32,address=00000001,group=2)
         -group 2 for slaves
         -group 0 for controller
         -group 1 for actors/feedback controllers
-
 """
 
-#Turn radio on
+# timer object to reset the microprocessor after every hour of running
+t = time.Timer(3600.0, reset)
+
+def detectLight():
+    if light < y:
+        yList.append(y) 
+        sleep(6000)
+    elif light == 0:
+        radio.send("ERROR, LIGHT")
+        sleep(6000)
+    else:
+        yList.append(y)
+        radio.send("ERROR, LOW LIGHT")
+        sleep(6000)
+
+def reportLight(ylist):
+    if len(ylist) == 60:
+        radio.send(yList)
+        ylist = []
+
+def detectWater():
+    # Water probe is on pin 1
+    water = pin1.read_analog()
+    """ If water level below x, do:
+    Probe is mesuring the resistance caused by the soil
+    hence the backwards operands '</>' """
+    if water < 0:
+        if water < x:
+            radio.send(water)
+        elif water >= x:
+            radio.send(water)
+            display.scroll("Water me!")
+    
+
+# Turn radio on
 radio.on()
 
-#Var x used for water limit, out of possible 1024
-x = 450
-#Var y used for light levels, out of possible 1024
-y = 100
-yList = []
-
-#Use async to make this a seperate thread in future
 while True:
-    #listen for reset command
+    t.start()
+    """ Two threads here for detecting and reporting light levels
+    this may be superfluous and cause thread lock"""
+    thread.start_new_thread(detectLight())
+    thread.start_new_thread(reportLight())
+    detectWater()
+    # listen for reset command
     ans = radio.receive()
-    #Water probe is on pin 1
-    water = pin1.read_analog()
     light = pin0.read_analog()
     
-    #If water level below x, do:
-    if water < x:
-        radio.send(water)
-    elif water >= x:
-        display.scroll("Water me!")
-        radio.send(water)
-    
-    """
-    #Lines 46 - 56 need to become thread independent and safe
-    if light < y:
-        ylist.append(y)
-        wait(60000)
-        radio.send("y: ",y)
-    elif light == 0:
-        wait(60000)
-        radio.send("ERROR, LIGHT")
-    else:
-        ylist.append(y)
-        wait(60000)
-        radio.send("ERROR, LOW LIGHT")
-        """
-    
-    #Needs boolean to detect water existance (if water == True)
     if ans == 'reset':
         display.scroll("CYCLING")
         reset()
